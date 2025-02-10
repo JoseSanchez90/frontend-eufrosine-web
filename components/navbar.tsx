b8135a715586c530ebb550d50fad2d602215f970
Jose Sanchez <josesancheztrujillo90@hotmail.com>
2025-02-10 12:46:26 -0500

    change navbar all

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { ShoppingCart, User } from "lucide-react";
 import { useRouter } from "next/navigation";
 import MenuList from "./menu-list";
 import ItemsMenuMobile from "./items-menu-mobile";
@@ -11,7 +10,7 @@ const Navbar = () => {
     const router = useRouter()
 
     return ( 
-        <div className="flex flex-row justify-between items-center p-4 md:px-48 cursor-pointer">
+        <div className="flex flex-row justify-between items-center p-4 cursor-pointer">
             <h1 className="text-2xl text-blue-800 dark:text-blue-600 font-normal" onClick={() => router.push("/")}>Eu<span className="text-3xl font-bold">Frosine</span></h1>
             <div className="items-center justify-between hidden sm:flex">
                 <MenuList/>
@@ -19,9 +18,7 @@ const Navbar = () => {
             <div className="flex sm:hidden">
                 <ItemsMenuMobile/>
             </div>
-            <div className="flex items-center justify-between gap-2 sm:gap-7">
-                <User strokeWidth={1} className="cursor-pointer" onClick={() => router.push("/usuario")}/>
-                <ShoppingCart strokeWidth={1} className="cursor-pointer" onClick={() => router.push("/carrito-compras")}/>
+            <div className="flex flex-row items-center justify-between gap-2 sm:gap-7">
                 <ToggleTheme/>
             </div>
         </div>
