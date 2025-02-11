3886b7720abd8fd2ce6f7ab5ff4358869d34e4f6
Jose Sanchez <josesancheztrujillo90@hotmail.com>
2025-02-11 09:41:52 -0500

    update popovercontent

--- components/items-menu-mobile.tsx
+++ components/items-menu-mobile.tsx
@@ -15,10 +15,10 @@ const ItemsMenuMobile = () => {
             </PopoverTrigger>
             <PopoverContent onClick={() => setOpen(!open)}>
                 <div className="flex flex-col gap-2">
-                    <Link href="/nosotros" className="block">Quienes Somos</Link>
-                    <Link href="/mision-vision" className="block">Mision / Vision</Link>
-                    <Link href="/blog" className="block">Blog</Link>
-                    <Link href="/nuestros-productos" className="block">Nuestros Productos</Link>
+                    <Link href="/nosotros" className="block" onClick={closePopover}>Quienes Somos</Link>
+                    <Link href="/mision-vision" className="block" onClick={closePopover}>Mision / Vision</Link>
+                    <Link href="/blog" className="block" onClick={closePopover}>Blog</Link>
+                    <Link href="/nuestros-productos" className="block" onClick={closePopover}>Nuestros Productos</Link>
                 </div>
             </PopoverContent>
         </Popover>
