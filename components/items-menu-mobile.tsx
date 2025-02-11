7e5e770aab7cb666669a2b7522d9600e945b24e1
Jose Sanchez <josesancheztrujillo90@hotmail.com>
2025-02-11 08:30:18 -0500

    about create Link

--- components/items-menu-mobile.tsx
+++ components/items-menu-mobile.tsx
@@ -10,7 +10,7 @@ const ItemsMenuMobile = () => {
             </PopoverTrigger>
             <PopoverContent>
                 <div className="flex flex-col gap-2">
-                    <Link href="/quienes-somos" className="block">Quienes Somos</Link>
+                    <Link href="/nosotros" className="block">Quienes Somos</Link>
                     <Link href="/mision-vision" className="block">Mision / Vision</Link>
                     <Link href="/blog" className="block">Blog</Link>
                     <Link href="/nuestros-productos" className="block">Nuestros Productos</Link>
